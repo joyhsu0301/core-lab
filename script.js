3eeb1af09ed068c5c4b9a696c0459690d76461a7
joyhsu0301 <60234925+joyhsu0301@users.noreply.github.com>
2020-02-08 16:32:53 -0500

    updated my homepage

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
 	//alert('testing');
+
+	//shadows under gifs
+	$('.reading').bind('mouseenter', function(){
+		$('.shadow').addClass('shadowr');
+		$('.textr').addClass('textrr');
+	})
+
+	$('.reading').bind('mouseleave', function(){
+		$('.shadow').removeClass('shadowr');
+		$('.textr').removeClass('textrr');
+	})
+ 
+ 	$('.homework').bind('mouseenter', function(){
+		$('.shadow').addClass('shadowh');
+		$('.texth').addClass('texthh');
+	})
+
+	$('.homework').bind('mouseleave', function(){
+		$('.shadow').removeClass('shadowh');
+		$('.texth').removeClass('texthh');
+	})
 	
+
 });
