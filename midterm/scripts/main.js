45e671878fece30a8f5e9aacdf341433ecfdaec3
joyhsu0301 <60234925+joyhsu0301@users.noreply.github.com>
2020-03-31 18:44:15 +0800

    added more onto the browser extension

--- midterm/scripts/main.js
+++ midterm/scripts/main.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
 	$('.contact-us-nav').remove();
 	$('footer').remove();
 
+	$('.col-md-12').on('mouseenter', function(){
+		$('.hide-mobile').hide();
+	});
+	$('.col-md-12').on('mouseleave', function(){
+		$('.hide-mobile').show();
+	})
+
 	$(document).on('scroll', function(){
 		$('.hide-mobile').css({'padding':'0px'});
 		$('.hide-mobile').ready(function(){
@@ -57,22 +64,80 @@ $(document).ready(function(){
 		var iphone2=document.createElement('img');
 		iphone2.style.zIndex='5';
 		iphone2.style.position='absolute';
-		iphone2.style.top='600px';
+		iphone2.style.top='500px';
 		iphone2.style.left='560px';
 		iphone2.style.width='1000px';
 		$(iphone2).attr({'src':'https://i.ya-webdesign.com/images/blank-smartphone-png.png', 'srcset':'https://i.ya-webdesign.com/images/blank-smartphone-png.png'});
 		document.querySelector('.social-proof-wrapper').appendChild(iphone2);
+		
+		$(iphone2).on('mouseenter', function(){
+		var note1=document.createElement('img');
+		note1.style.zIndex='4';
+		note1.style.position='absolute';
+		note1.style.top='590px';
+		note1.style.left='650px';
+		note1.style.width='418px';
+		note1.style.height='280px';
+		$(note1).attr({'src':'https://d2w9rnfcy7mm78.cloudfront.net/6659081/large_a699a9814cb4aad92d24b5b1f6a34d56.jpg?1585648984?bc=0', 'srcset':'https://d2w9rnfcy7mm78.cloudfront.net/6659081/large_a699a9814cb4aad92d24b5b1f6a34d56.jpg?1585648984?bc=0'})
+			.hide()
+			.fadeIn('200');
+		document.querySelector('.social-proof-wrapper').appendChild(note1);
+		});
+
+		var iphone3=document.createElement('img');
+		iphone3.style.zIndex='5';
+		iphone3.style.position='absolute';
+		iphone3.style.top='1000px';
+		iphone3.style.left='230px';
+		iphone3.style.width='900px';
+		$(iphone3).attr({'src':'https://d2w9rnfcy7mm78.cloudfront.net/6657974/large_65abc79f6c82625c9a44d75b1a523fc9.png?1585638214?bc=0', 'srcset':'https://d2w9rnfcy7mm78.cloudfront.net/6657974/large_65abc79f6c82625c9a44d75b1a523fc9.png?1585638214?bc=0'});
+		document.querySelector('.social-proof-wrapper').appendChild(iphone3);
+
+		$(iphone3).on('mouseenter', function(){
+		var note2=document.createElement('img');
+		note2.style.zIndex='4';
+		note2.style.position='absolute';
+		note2.style.top='1070px';
+		note2.style.left='632px';
+		note2.style.width='244px';
+		note2.style.height='477px';
+		$(note2).attr({'src':'https://d2w9rnfcy7mm78.cloudfront.net/6658667/large_0669d9071e1099de5a3bf3c16f40de37.png?1585646374?bc=0', 'srcset':'https://d2w9rnfcy7mm78.cloudfront.net/6658667/large_0669d9071e1099de5a3bf3c16f40de37.png?1585646374?bc=0'})
+			.hide()
+			.fadeIn('200');
+		document.querySelector('.social-proof-wrapper').appendChild(note2);
+		});
 	});
 
 	$(document).on('scroll', function(){
-		$('h1').css({'text-decoration':'line-through'});
+		$('h1').hide();
+
+		var textbox=document.createElement('h1');
+		var newtext=document.createTextNode('But first, you need to learn some');
+		textbox.style.zIndex='6';
+		textbox.style.position='absolute';
+		textbox.style.left='150px';
+		textbox.style.top='-10px';
+		textbox.style.textDecoration='none';
+		textbox.appendChild(newtext);
+		document.querySelector('.headline').append(textbox);
+		
+		var etiquette=document.createElement('img');
+		etiquette.style.zIndex='7';
+		etiquette.style.position='absolute';
+		etiquette.style.left='240px';
+		etiquette.style.top='245px';
+		etiquette.style.width='260px';
+		$(etiquette).attr({'src':'https://d2w9rnfcy7mm78.cloudfront.net/6657887/large_abd027a7ba8082f6194ff630e43da71c.png?1585637599?bc=0', 'srcset':'https://d2w9rnfcy7mm78.cloudfront.net/6657887/large_abd027a7ba8082f6194ff630e43da71c.png?1585637599?bc=0'})
+			.hide()
+			.fadeIn('200');
+		document.querySelector('.headline').append(etiquette);
+
 		var logo=document.createElement('img');
 		logo.style.zIndex='6';
 		logo.style.position='absolute';
 		logo.style.left='80px';
-		logo.style.top='-5px';
+		logo.style.top='0px';
 		logo.style.width='500px';
-		logo.style.transform='rotate(-25deg)';
 		$(logo).attr({'src':'https://cdn1.venmo.com/marketing/images/branding/downloads/venmo_logo_blue.svg', 'srcset':'https://cdn1.venmo.com/marketing/images/branding/downloads/venmo_logo_blue.svg'})
 			.hide()
 			.fadeIn('200');
