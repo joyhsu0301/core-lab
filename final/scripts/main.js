b833407f33691aecc21c64407746a06eb11bf45a
joyhsu0301 <60234925+joyhsu0301@users.noreply.github.com>
2020-05-02 03:28:43 +0800

    added images to each object in array and appended them on click of scents

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -5,63 +5,81 @@ $(document).ready(function(){
 			'name': 'Beijing Home',
 			'color': 'peru',
 			'feeling': 'nostalgia',
-			'image': 'https://www.pngfind.com/pngs/m/357-3570347_aesthetic-clipart-tumblr-transparent-overlay-aesthetic-stars-png.png'
+			'image': 'https://pngimg.com/uploads/sofa/sofa_PNG6955.png',
+			'width': '100',
+			'height': '50'
 		},
 
 		{
 			'name': 'Taipei Home',
 			'color': 'blanchedalmond',
 			'feeling': 'vacation',
-			'image': 'https://www.pngfind.com/pngs/m/357-3570347_aesthetic-clipart-tumblr-transparent-overlay-aesthetic-stars-png.png'
+			'image': 'http://www.pngmart.com/files/11/Firewood-Sacked-Transparent-PNG.png',
+			'width': '100',
+			'height': '66'
 		},
 
 		{
 			'name': 'New York Home',
 			'color': 'beige',
 			'feeling': 'comfort',
-			'image': 'https://www.pngfind.com/pngs/m/357-3570347_aesthetic-clipart-tumblr-transparent-overlay-aesthetic-stars-png.png'
+			'image': 'images/ein.png',
+			'width': '76',
+			'height': '100'
 		},
 
 		{
 			'name': 'Grandmas House',
 			'color': 'sienna',
 			'feeling': 'nostalgia',
-			'image': 'https://www.pngfind.com/pngs/m/357-3570347_aesthetic-clipart-tumblr-transparent-overlay-aesthetic-stars-png.png'
+			'image': 'images/buddha.png',
+			'width': '80',
+			'height': '100'
 		},
 
 		{
 			'name': 'Junyis House',
 			'color': 'cornsilk',
 			'feeling': 'happy',
-			'image': 'https://www.pngfind.com/pngs/m/357-3570347_aesthetic-clipart-tumblr-transparent-overlay-aesthetic-stars-png.png'
+			'image': 'images/plates.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'Kindergarten',
 			'color': 'lightblue',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/kid.png',
+			'width': '65',
+			'height': '100'
 		},
 
 		{
 			'name': 'Home Clothes',
 			'color': 'palevioletred',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/tshirt.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'Haneda Airport',
 			'color': 'white',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/tiat.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'JFK Airport',
 			'color': 'white',
 			'feeling': 'sad',
-			'image': ''
+			'image': 'images/hands.png',
+			'width': '100',
+			'height': '80'
 		},
 
 
@@ -69,189 +87,243 @@ $(document).ready(function(){
 			'name': 'Color Pencils',
 			'color': 'darkred',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/pencils.png',
+			'width': '60',
+			'height': '100'
 		},
 
 		{
 			'name': '7-Eleven',
 			'color': 'green',
 			'feeling': 'otherworldly',
-			'image': ''
+			'image': 'images/oden.png',
+			'width': '100',
+			'height': '80'
 		},
 
 		{
 			'name': 'Temple Incense',
 			'color': 'darkred',
 			'feeling': 'otherworldly',
-			'image': ''
+			'image': 'images/incense.png',
+			'width': '70',
+			'height': '100'
 		},
 
 		{
 			'name': 'Onsen',
 			'color': 'lightyellow',
 			'feeling': 'relaxed',
-			'image': ''
+			'image': 'https://b.kisscc0.com/20190326/qaq/kisscc0-hot-spring-computer-icons-onsen-spa-red-onsen-mark-5c9a3803b07ac2.7596500315536107557229.png',
+			'width': '100',
+			'height': '70'
 		},
 
 		{
 			'name': 'Dads Car',
 			'color': 'blanchedalmond',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/car.png',
+			'width': '70',
+			'height': '100'
 		},
 
 		{
 			'name': 'Cardboard',
 			'color': 'peru',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/cardboard.png',
+			'width': '100',
+			'height': '50'
 		},
 
 		{
 			'name': 'Manga Collection',
 			'color': 'lightgray',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'https://upload.wikimedia.org/wikipedia/en/5/50/Cardcaptor_Sakura_vol1_cover.jpg',
+			'width': '65',
+			'height': '100'
 		},
 
 		{
 			'name': 'Swimming Pool',
 			'color': 'deepskyblue',
 			'feeling': 'vacation',
-			'image': ''
+			'image': 'https://pluspng.com/img-png/swimming-goggles-png-swb-1m-sm-y-png-842.png',
+			'width': '100',
+			'height': '60'
 		},
 
 		{
 			'name': 'NYC streets',
 			'color': 'darkslategray',
 			'feeling': 'gross',
-			'image': ''
+			'image': 'images/trash.png',
+			'width': '90',
+			'height': '100'
 		},
 
 		{
 			'name': 'Chinese New Years Dinner',
 			'color': 'crimson',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/fish.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'Bamboo Forest',
 			'color': 'limegreen',
 			'feeling': 'relaxed',
-			'image': ''
+			'image': 'images/bamboo.png',
+			'width': '70',
+			'height': '100'
 		},
 
 		{
 			'name': 'Tatami',
 			'color': 'limegreen',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/table.png',
+			'width': '100',
+			'height': '75'
 		},
 
 		{
 			'name': 'My Wallet',
 			'color': 'saddlebrown',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/wallet.png',
+			'width': '100',
+			'height': '65'
 		},
 
 		{
 			'name': 'Warm Souffle',
 			'color': 'burlywood',
 			'feeling': 'happy',
-			'image': ''
+			'image': 'images/souffle.png',
+			'width': '80',
+			'height': '80'
 		},
 
 		{
 			'name': 'Takeout Thai Food',
 			'color': 'limegreen',
 			'feeling': 'hungry',
-			'image': ''
+			'image': 'images/box.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'Makeup',
 			'color': 'dimgray',
 			'feeling': 'sad',
-			'image': ''
+			'image': 'images/makeup.png',
+			'width': '140',
+			'height': '100'
 		},
 
 		{
 			'name': 'Shakshuka',
 			'color': 'firebrick',
 			'feeling': 'hungry',
-			'image': ''
+			'image': 'images/shakshuka.png',
+			'width': '80',
+			'height': '80'
 		},
 
 		{
 			'name': 'Mom',
 			'color': 'purple',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Jewelry-and-Diamonds-PNG/Transparent_Wedding_Rings_Clipart.png?m=1507172105',
+			'width': '80',
+			'height': '80'
 		},
 
 		{
 			'name': 'Dad',
 			'color': 'darkseagreen',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/cardholder.png',
+			'width': '100',
+			'height': '100'
 		},
 
 		{
 			'name': 'Cigarettes',
 			'color': 'gray',
 			'feeling': 'relaxed',
-			'image': ''
+			'image': 'images/melvius.png',
+			'width': '60',
+			'height': '90'
 		},
 
 		{
 			'name': 'Vodka',
 			'color': 'white',
 			'feeling': 'gross',
-			'image': ''
+			'image': 'images/vodka.png',
+			'width': '50',
+			'height': '110'
 		},
 
 		{
 			'name': 'Favorite Hand Soap',
 			'color': 'white',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'https://cdn5.hermo.my/product_images/malls/42915_senka-perfect-whip-for-body-500ml_440_280_1564998412.png',
+			'width': '180',
+			'height': '100'
 		},
 
 		{
 			'name': 'Aesop Hand Cream',
 			'color': 'green',
 			'feeling': 'happy',
-			'image': ''
+			'image': 'images/lotion.png',
+			'width': '110',
+			'height': '110'
 		}, 
 
 		{
 			'name': '75% Alcohol Spray',
 			'color': 'white',
 			'feeling': 'comfort',
-			'image': ''
+			'image': 'images/spray.png',
+			'width': '70',
+			'height': '120'
 		}, 
 
 		{
 			'name': 'Oil Paint',
 			'color': 'oldlace',
 			'feeling': 'nostalgia',
-			'image': ''
+			'image': 'images/gamsol.png',
+			'width': '100',
+			'height': '100'
 		}, 
 
 		{
 			'name': 'Mint',
 			'color': 'limegreen',
 			'feeling': 'relaxed',
-			'image': ''
+			'image': 'images/mint.png',
+			'width': '35',
+			'height': '70'
 		},
 
 		{
 			'name': 'Scrunched Up Tissue',
 			'color': 'oldlace',
 			'feeling': 'indifferent',
-			'image': ''
+			'image': 'https://lh3.googleusercontent.com/proxy/BrYsdU77qpIdVpMx4O4qrF0xaA7sKRKY7iOtqew20KpBdXU9jbjGcrJRu-dEWVcXwedVBERrMh8Pb2WghsZ2hTrSmWRlqC347XH8Tr8-WzMgH-543DA',
+			'width': '100',
+			'height': '100'
 		}
 
 	]
@@ -293,7 +365,7 @@ $(document).ready(function(){
 		.click(function(){
 			positiontop = Math.random()*1000;
 			positionleft = Math.random()*1000;
-			$('#image_farm').append('<img class="scents_image" style="top: '+positiontop+'px; left: '+positionleft+'px; position: fixed; src="' + $(this).data("randomscent").image + '">');
+			$('#image_farm').append('<img src="'+$(this).data("randomscent").image+'" style="top: '+positiontop+'px; left: '+positionleft+'px; width: '+$(this).data("randomscent").width+'px; height: '+$(this).data("randomscent").height+'px; position: fixed;">');
 		});
 		$('#random_items').append(scentDiv);
 	}
@@ -326,6 +398,7 @@ $(document).ready(function(){
 		$('#grid_items').css({'display': 'none'});
 		$('#color_items').css({'display': 'none'});
 		$('#image_farm').css({'display': 'inline'});
+		$('#image_farm').empty();
 	});
 
 	//filter
@@ -393,13 +466,13 @@ $(document).ready(function(){
 					$('#scent_name').remove();
 				})
 				.click(function(){
-				positiontop = Math.random()*1000;
-				positionleft = Math.random()*1000;
-				$('#image_farm').append('<img class="scents_image" style="top: '+positiontop+'px; left: '+positionleft+'px; position: fixed; src="' + $(this).data("scents").image + '">');
-				});
-				$('#items').append(scentsDiv);
-				$(this).data('clicked', true);
-			}
+					positiontop = Math.random()*1000;
+					positionleft = Math.random()*1000;
+					$('#image_farm').append('<img src="'+$(this).data("scents").image+'" style="top: '+positiontop+'px; left: '+positionleft+'px; width: '+$(this).data("scents").width+'px; height: '+$(this).data("scents").height+'px; position: fixed;">');
+						});
+						$('#items').append(scentsDiv);
+						$(this).data('clicked', true);
+					}
 		} else{
 			$('body').css({'background-color':'white'});
 		}
